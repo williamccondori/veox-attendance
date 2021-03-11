f3b4868b6e720b45d0be896a4afa8ac17de2b315
William Condori Quispe <williamccondori@gmail.com>
2021-03-11 13:13:37 -0500

    Add configuration

--- Veox.Attendance.Record/Veox.Attendance.Record.Api/Startup.cs
+++ Veox.Attendance.Record/Veox.Attendance.Record.Api/Startup.cs
@@ -17,7 +17,7 @@ namespace Veox.Attendance.Record.Api
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         /// <summary>
         /// This method gets called by the runtime. Use this method to add services to the container.
