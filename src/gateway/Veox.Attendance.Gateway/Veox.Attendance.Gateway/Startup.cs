f1abaed58dce4233dce5309e9be46f5b3e26e97e
William Condori Quispe <williamccondori@outlook.com>
2021-03-18 16:27:36 -0500

    Add scripts

--- src/gateway/Veox.Attendance.Gateway/Veox.Attendance.Gateway/Startup.cs
+++ src/gateway/Veox.Attendance.Gateway/Veox.Attendance.Gateway/Startup.cs
@@ -15,6 +15,7 @@ namespace Veox.Attendance.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddOcelot().AddConsul();
         }
 
@@ -32,6 +33,8 @@ namespace Veox.Attendance.Gateway
             {
                 endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
             });
+            
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseOcelot().Wait();
         }
